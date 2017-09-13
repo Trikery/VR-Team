585a4db3423291d30651ebd153ff89b19e809322
Ben <Fuzzinator12@gmail.com>
2017-09-12 18:46:31 -0600

    6th test

--- VR Multiplayer III/Assets/Scripts/Mover.cs
+++ VR Multiplayer III/Assets/Scripts/Mover.cs
@@ -92,10 +92,10 @@ public class Mover : NetworkBehaviour {
 
             //controls transformations
             characterRigid.MovePosition(transform.localPosition + transform.TransformDirection(//new Vector3(/*controller.touchSpot.x*/0 , 0, forward/*controller.touchSpot.y*/)
-                controller.cameraHead.transform.forward * -1) * controller.moveSpeed * Time.deltaTime);
+                controller.cameraHead.transform.right * -1) * controller.moveSpeed * Time.deltaTime);
 
             //controls rotation
-            newRotate = Mathf.Atan2(controller.device.GetAxis().y, (controller.device.GetAxis().x *-1)) * Mathf.Rad2Deg +90;
+            newRotate = Mathf.Atan2(controller.device.GetAxis().y, (controller.device.GetAxis().x *-1)) * Mathf.Rad2Deg +0;
             Quaternion tempRotate = Quaternion.Euler(0, newRotate, 0);    
             transform.localRotation = Quaternion.Slerp(transform.rotation, tempRotate, Time.deltaTime * controller.rotateSpeed);
 
