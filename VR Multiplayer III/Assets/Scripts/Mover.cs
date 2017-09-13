d8a58eda3a29938bdebefad42d367dd097ed1157
Ben <Fuzzinator12@gmail.com>
2017-09-13 11:36:15 -0600

    Mover script re added
    lets try again

---
+++ VR Multiplayer III/Assets/Scripts/Mover.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Mover : NetworkBehaviour {
+
+    public static Action</*string,*/ Controller> CallMover;
+    public static GameObject thisDino;
+
+    Controller controller;
+
+    private float newRotate;
+    private Quaternion focusRotate;
+    private Vector3 moveRotate;
+    private Rigidbody characterRigid;
+    private float forward;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        if (!isLocalPlayer)
+        {
+            Destroy(this);
+            return;
+        }
+
+        thisDino = gameObject;
+
+        characterRigid = gameObject.GetComponent<Rigidbody>();
+        CallMover += CallMoverHandler;
+    }
+
+    private void CallMoverHandler(/*string command,*/ Controller mover)
+    {
+        controller = mover;
+        if (controller.touching || controller.gripped)
+        {
+            StartCoroutine(MoveController());
+        }
+        if(controller.clicked)
+        {
+            StartCoroutine(Jump());
+            StartCoroutine(JumpCount());
+            StartCoroutine(ResetJump());
+
+        }
+        if(controller.pulledTrigger)
+        {
+            //StartCoroutine(ShootGun());
+
+        }
+            //switch(command)
+        //{
+        //    case "Touching":
+                
+        //        StartCoroutine(MoveController());
+        //        break;
+        //    case "Not Touching":
+        //        break;
+        //    default:
+        //        return;
+
+        //}
+    }
+
+
+    IEnumerator MoveController()
+    {
+        yield return new WaitForFixedUpdate();
+        controller.device = SteamVR_Controller.Input((int)controller.trackedObject.index);
+        controller.touchSpot = new Vector2(controller.device.GetAxis().x, controller.device.GetAxis().y);
+        
+
+        
+
+        if (controller.touchSpot != new Vector2(0, 0) && !controller.gripped)
+        {
+            //set input value to always be positive
+            if (controller.touchSpot != null)
+            {
+                forward = controller.device.GetAxis().x + controller.device.GetAxis().y;
+                if (forward < 0)
+                {
+                    forward *= -1;
+                }
+                if (forward > 1)
+                {
+                    forward *= .5f;
+                }
+            }
+
+            //controls transformations
+            characterRigid.MovePosition(transform.localPosition + transform.TransformDirection(new Vector3(0 , 0, forward)
+                ) * controller.moveSpeed * Time.deltaTime);
+
+            //controls rotation
+            newRotate = Mathf.Atan2(controller.device.GetAxis().y * -1, (controller.device.GetAxis().x )) * Mathf.Rad2Deg;
+            Quaternion tempRotate = Quaternion.Euler(0, newRotate - ((controller.cameraHead.transform.localEulerAngles.y -90) * -1), 0);    
+            transform.localRotation = Quaternion.Slerp(transform.rotation, tempRotate, Time.deltaTime * controller.rotateSpeed);
+
+        }
+
+        if (controller.gripped)
+        {
+            //controlls rotation
+            focusRotate = Quaternion.LookRotation(controller.focus.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, focusRotate, Time.deltaTime * controller.rotateSpeed);
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+            //controlls transformation
+            characterRigid.MovePosition(transform.localPosition + transform.TransformDirection
+                (new Vector3(controller.touchSpot.x, 0, controller.touchSpot.y)) * controller.moveSpeed * Time.deltaTime);
+
+        }
+        if (controller.touching)
+            StartCoroutine(MoveController());
+
+    }
+
+    IEnumerator Jump()
+    {
+        yield return new WaitForFixedUpdate();
+        transform.Translate(Vector3.up * controller.jumpSpeed * Time.deltaTime);
+        if (controller.frameCount < controller.jumpAmount)
+            StartCoroutine(Jump());
+    }
+
+    IEnumerator JumpCount()
+    {
+        yield return new WaitForSeconds(.5f);
+        controller.frameCount++;
+        if (controller.frameCount < controller.jumpAmount)
+            StartCoroutine(JumpCount());
+    }
+
+    IEnumerator ResetJump()
+    {
+        yield return new WaitForSeconds(2);
+        controller.frameCount = 0;
+    }
+
+}
+
+
