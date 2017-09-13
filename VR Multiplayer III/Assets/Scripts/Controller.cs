6b936c45757774fa01373ad772ad0bf94d61f680
Ben <Fuzzinator12@gmail.com>
2017-09-13 17:20:18 -0600

    jumping works well
    jump only once but every time you touch the ground you can jump again, and it smoothly transitions between jumping and walking

--- VR Multiplayer III/Assets/Scripts/Controller.cs
+++ VR Multiplayer III/Assets/Scripts/Controller.cs
@@ -21,6 +21,7 @@ public class Controller : NetworkBehaviour {
     public float bulletSpeed = 500;
     public float frameCount;
     public float jumpAmount;
+    public float forwardJmpSpeed;
     public bool touching;
     public bool pulledTrigger;
     public bool gripped;
--- VR Multiplayer III/Assets/Scripts/Mover.cs
+++ VR Multiplayer III/Assets/Scripts/Mover.cs
@@ -38,20 +38,28 @@ public class Mover : NetworkBehaviour {
     private void CallMoverHandler(/*string command,*/ Controller mover)
     {
         controller = mover;
-        if (controller.touching || controller.gripped)
-        {
-            StartCoroutine(MoveController());
-        }
-        if(controller.clicked && !jumping)
+        controller.jumpSpeed = 2000;
+        controller.forwardJmpSpeed = 300;
+        if (controller.clicked && !jumping)
         {
             //characterRigid.AddForce(Vector3.up * Time.deltaTime * controller.jumpSpeed);
             StartCoroutine(Jump());
-            StartCoroutine(ForwardForce());
+           // StartCoroutine(ForwardForce());
             StartCoroutine(JumpCount());
             StartCoroutine(ResetJump());
             jumping = true;
+            StopCoroutine(MoveController());
 
         }
+        if (controller.touching || controller.gripped)
+        {
+            if (!controller.clicked && !jumping)
+            {
+                StartCoroutine(MoveController());
+            }
+            
+        }
+        
         if(controller.pulledTrigger)
         {
             //StartCoroutine(ShootGun());
@@ -119,7 +127,7 @@ public class Mover : NetworkBehaviour {
                 (new Vector3(controller.touchSpot.x, 0, controller.touchSpot.y)) * controller.moveSpeed * Time.deltaTime);
 
         }
-        if (controller.touching)
+        if (controller.touching && !controller.clicked)
             StartCoroutine(MoveController());
 
     }
@@ -128,18 +136,19 @@ public class Mover : NetworkBehaviour {
     {
         yield return new WaitForFixedUpdate();
         characterRigid.AddForce(Vector3.up * controller.jumpSpeed * Time.deltaTime);
+        characterRigid.AddForce(transform.forward * controller.moveSpeed * controller.forwardJmpSpeed * Time.deltaTime);
         //transform.Translate(Vector3.up * controller.jumpSpeed * Time.deltaTime);
         if (controller.frameCount < controller.jumpAmount)
             StartCoroutine(Jump());
     }
-    IEnumerator ForwardForce()
-    {
-        characterRigid.AddForce(transform.forward * controller.moveSpeed * 400 * Time.deltaTime);
-        //characterRigid.MovePosition(transform.localPosition + transform.TransformDirection(new Vector3(0, 0, forward)) * controller.moveSpeed * 4 * Time.deltaTime);
-        yield return new WaitForFixedUpdate();
-        if (jumping)
-            StartCoroutine(ForwardForce());
-    }
+    //IEnumerator ForwardForce()
+    //{
+        
+    //    //characterRigid.MovePosition(transform.localPosition + transform.TransformDirection(new Vector3(0, 0, forward)) * controller.moveSpeed * 4 * Time.deltaTime);
+    //    yield return new WaitForFixedUpdate();
+    //    if (jumping)
+    //        StartCoroutine(ForwardForce());
+    //}
 
     IEnumerator JumpCount()
     {
@@ -154,6 +163,10 @@ public class Mover : NetworkBehaviour {
         yield return new WaitForSeconds(1f);
         controller.frameCount = 0;
         jumping = false;
+        if (controller.touching || controller.gripped)
+        {
+            StartCoroutine(MoveController());
+        }
     }
 
 }
