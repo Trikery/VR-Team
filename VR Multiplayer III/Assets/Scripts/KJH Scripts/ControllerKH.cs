1336c57313df7dacb2245c664eea01596065646d
Kirt Hullinger <kirt.jh@gmail.com>
2017-09-16 19:02:10 -0600

    Lockon Weapon

---
+++ VR Multiplayer III/Assets/Scripts/KJH Scripts/ControllerKH.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ControllerKH : MonoBehaviour {
+
+    public SteamVR_TrackedController _controller;
+    public SteamVR_TrackedObject trackedObject;
+    public SteamVR_Controller.Device device;
+    public Vector2 touchSpot;
+    public GameObject dino;
+    public GameObject bullet;
+    public GameObject cameraHead;
+    public Transform nose;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float jumpSpeed = 5;
+    public float bulletSpeed = 500;
+    public float frameCount;
+    public float jumpAmount;
+    public float forwardJmpSpeed;
+    public bool touching;
+    public bool pulledTrigger;
+    public bool gripped;
+
+    public Transform focus;
+    
+    public bool clicked;
+
+
+    private void OnEnable()
+    {
+
+        _controller = GetComponent<SteamVR_TrackedController>();
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+        _controller.TriggerClicked += HandleTriggerClicked;
+        _controller.TriggerUnclicked += HandleTriggerUnclicked;
+        _controller.PadTouched += LPadTouched;
+        _controller.PadUntouched += LPadUntouched;
+        _controller.PadClicked += RHandlePadClicked;
+        _controller.PadUnclicked += RHandlePadClickUp;
+        _controller.Gripped += HandleGripped;
+        _controller.Ungripped += HandleUngripped;
+        touching = false;
+        pulledTrigger = false;
+        clicked = false;
+
+        dino = Mover.thisDino;
+
+        //_controller.TriggerUnclicked += HandleTriggerUnclicked;
+        //_controller.MenuButtonClicked += MenuButtonHandler;
+        //following = false;
+        //melee = false;
+        //_controller.Ungripped += HandleUngripped;            
+        }
+
+
+    private void HandleUngripped(object sender, ClickedEventArgs e)
+    {
+        gripped = false;
+    }
+
+    private void HandleGripped(object sender, ClickedEventArgs e)
+    {
+        gripped = true;
+        MoverKH.CallMover(this);
+		WeaponLockOn_KH.setFocus (this);
+    }
+
+    private void RHandlePadClickUp(object sender, ClickedEventArgs e)
+    {
+        clicked = false;
+        
+    }
+
+    private void RHandlePadClicked(object sender, ClickedEventArgs e)
+    {
+        
+        clicked = true;
+        MoverKH.CallMover(this);
+        //StartCoroutine(Jump());
+    }
+
+    private void LPadUntouched(object sender, ClickedEventArgs e)
+    {
+        touching = false;
+        
+        
+    }
+
+    private void LPadTouched(object sender, ClickedEventArgs e)
+    {
+        
+        touching = true;
+        MoverKH.CallMover(this);
+
+    }
+
+    private void HandleTriggerUnclicked(object sender, ClickedEventArgs e)
+    {
+        pulledTrigger = false;
+
+    }
+
+    private void HandleTriggerClicked(object sender, ClickedEventArgs e)
+    {
+        pulledTrigger = true;
+        Shoot.shooter();
+        //StartCoroutine(ShootGun());
+    }
+
+    private void OnDisable()
+    {
+        _controller.TriggerClicked -= HandleTriggerClicked;
+        _controller.TriggerUnclicked -= HandleTriggerUnclicked;
+        _controller.PadTouched -= LPadTouched;
+        _controller.PadUntouched -= LPadUntouched;
+        _controller.PadClicked -= RHandlePadClicked;
+        _controller.PadUnclicked -= RHandlePadClickUp;
+        _controller.Gripped -= HandleGripped;
+        _controller.Ungripped -= HandleUngripped;
+    }
+
+    //IEnumerator MoveController()
+    //{
+    //    yield return new WaitForFixedUpdate();
+    //    device = SteamVR_Controller.Input((int)trackedObject.index);
+    //    touchSpot = new Vector2(device.GetAxis().x, device.GetAxis().y);
+    //    if (touchSpot != new Vector2(0, 0))
+    //    {
+    //        dino.transform.Translate(0, 0, (touchSpot.y * moveSpeed) * Time.deltaTime);
+    //        dino.transform.Rotate(0, (touchSpot.x * rotateSpeed) * Time.deltaTime, 0);
+    //    }
+    //    if (touching)
+    //        StartCoroutine(MoveController());
+        
+    //}
+
+    //IEnumerator ShootGun()
+    //{
+    //    yield return new WaitForFixedUpdate();
+
+    //    bullet.transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+
+    //    yield return new WaitForSeconds(1);
+
+
+    //    bullet.transform.position = nose.GetComponent<Transform>().position;
+    //    bullet.transform.localEulerAngles = nose.localEulerAngles;
+    //}
+    
+    //IEnumerator Jump()
+    //{
+    //    yield return new WaitForFixedUpdate();
+    //    dino.transform.Translate(Vector3.up * jumpSpeed * Time.deltaTime);
+    //    if (clicked == true && frameCount < jumpAmount)
+    //        StartCoroutine(Jump());
+    //}
+
+
+
+    //For the sake of debuging on a non VR controller
+
+
+    /*void Update()
+    {
+        if(Input.GetButtonDown("Horizontal")|| Input.GetButtonDown("Vertical"))
+        {
+            touching = true;
+            Mover.CallMover(this); 
+        }
+        if (!Input.GetButtonDown("Horizontal")&& !Input.GetButtonDown("Vertical"))
+        {
+            touching = false;
+        }
+        if(Input.GetKeyDown("space"))
+        {
+            clicked = true;
+            Mover.CallMover(this);
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            clicked = false;
+        }
+        if(Input.GetButtonDown("Fire1"))
+        {
+            pulledTrigger = true;
+            Shoot.shooter();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            pulledTrigger = false;
+        }
+        if(Input.GetButtonDown("Fire2"))
+        {
+            gripped = true;
+            Mover.CallMover(this);
+        }
+        if (Input.GetButtonUp("Fire2"))
+        {
+            gripped = false;
+        }
+    }*/
+
+    
+}
---
+++ VR Multiplayer III/Assets/Scripts/KJH Scripts/WeaponLockOn_KH.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class WeaponLockOn_KH : MonoBehaviour {
+
+	public static Action <ControllerKH> setFocus;
+
+	LineRenderer line;
+	RaycastHit rayHit;
+
+	public bool canRaycast;
+	public float rayTimeLimit = 2f;
+
+	public void Start(){
+		setFocus += SetFocusHandler;
+		line = gameObject.AddComponent<LineRenderer> ();
+		line.enabled = false;
+	}
+
+	private void SetFocusHandler(ControllerKH _controller){
+		StartCoroutine (RaycastWeapon ());
+		StartCoroutine (RaycastCounter ());
+	}
+
+	IEnumerator RaycastWeapon(){
+		Ray ray = new Ray(transform.position, transform.up * -1);
+		if (Physics.Raycast (ray, out rayHit)) {
+			line.SetPosition (0, transform.position);
+			line.SetPosition (1, rayHit.point);
+			line.enabled = true;
+		}
+		yield return new WaitForFixedUpdate ();
+		if (canRaycast)
+			StartCoroutine(RaycastWeapon());
+	}
+
+	IEnumerator RaycastCounter (){
+		yield return new WaitForSeconds (rayTimeLimit);
+		canRaycast = false;
+		line.enabled = false;
+
+	}
+}
