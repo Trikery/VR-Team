dab6f5198c43f46a2d1d91403c415b0e63f2afd5
Kirt Hullinger <kirt.jh@gmail.com>
2017-09-19 15:51:44 -0600

    Added List to WeaponLockOn Script

--- VR Multiplayer III/Assets/Scripts/KJH Scripts/WeaponLockOn_KH.cs
+++ VR Multiplayer III/Assets/Scripts/KJH Scripts/WeaponLockOn_KH.cs
@@ -5,31 +5,46 @@ using System;
 
 public class WeaponLockOn_KH : MonoBehaviour {
 
-	public static Action <ControllerKH> setFocus;
+	public static Action <Controller> setFocus;
+	public static List <Transform> allFocus;
+
+	private Controller _controller;
 
 	LineRenderer line;
 	RaycastHit rayHit;
 
 	public bool canRaycast;
 	public float rayTimeLimit = 2f;
+	public float lineEndWidth = .5f;
 
 	public void Start(){
 		setFocus += SetFocusHandler;
 		line = gameObject.AddComponent<LineRenderer> ();
 		line.enabled = false;
+		line.startWidth = 0;
+		line.endWidth = lineEndWidth;
 	}
 
-	private void SetFocusHandler(ControllerKH _controller){
+	private void SetFocusHandler(Controller _getFocus){
+		canRaycast = true;
+		_controller = _getFocus;
+		allFocus.Clear ();
 		StartCoroutine (RaycastWeapon ());
 		StartCoroutine (RaycastCounter ());
 	}
 
 	IEnumerator RaycastWeapon(){
-		Ray ray = new Ray(transform.position, transform.up * -1);
-		if (Physics.Raycast (ray, out rayHit)) {
+		//Ray ray = new Ray(transform.position, transform.up * -1);
+
+		if (Physics.Raycast (transform.position, transform.up * -1, out rayHit)) {
 			line.SetPosition (0, transform.position);
 			line.SetPosition (1, rayHit.point);
 			line.enabled = true;
+
+			if (rayHit.rigidbody != null) {
+				_controller.focus = rayHit.collider.GetComponent<Transform> ();
+				allFocus.Add (_controller.focus);
+			}
 		}
 		yield return new WaitForFixedUpdate ();
 		if (canRaycast)
