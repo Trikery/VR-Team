3b12fc85c205210fbbc973f581ceed661796ace2
Kirt Hullinger <kirt.jh@gmail.com>
2017-09-23 16:38:37 -0600

    Added Scripts and Prefab for pauseMenu

---
+++ VR Multiplayer III/Assets/Scripts/KJH Scripts/MenuButton_KH.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MenuButton_KH : MonoBehaviour {
+
+	public static Action <Controller> openMenu;
+	public GameObject pauseMenu;
+	public bool _menuIsOn = false;
+
+	void Start (){
+		_menuIsOn = false;
+		pauseMenu.SetActive (false);
+		openMenu += OpenMenuHandler;
+	}
+
+	private void OpenMenuHandler(Controller _menu){
+		ActivateMenu ();
+	}
+
+	public void ActivateMenu (){
+		if (_menuIsOn == false) {
+			pauseMenu.SetActive (true);
+			_menuIsOn = true;
+		}
+	}
+
+}
